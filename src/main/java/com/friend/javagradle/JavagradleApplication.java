4f425b379d7f79018ffa0cee60b47bf303254692
cacalos <cacalos@uangel.com>
2021-04-08 10:36:03 +0900

    init

---
+++ src/main/java/com/friend/javagradle/JavagradleApplication.java
@@ -0,0 +1,14 @@
+package com.friend.javagradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavagradleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/friend/javagradle/JavagradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friend.javagradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavagradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
